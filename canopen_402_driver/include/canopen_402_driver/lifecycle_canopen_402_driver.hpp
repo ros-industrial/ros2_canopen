78911fbb27cc8c1235a64900524f5d4a2a609acd
Christoph Hellmann Santos <christoph.hellmann.santos@ipa.fraunhofer.de>
2022-08-12 08:16:50 +0200

    Restructured canopen_402_driver for lifecycle support

--- canopen_402_driver/include/canopen_402_driver/lifecycle_canopen_402_driver.hpp
+++ canopen_402_driver/include/canopen_402_driver/lifecycle_canopen_402_driver.hpp
@@ -1,3 +1,4 @@
+
 #ifndef MC_DEVICE_NODE_HPP
 #define MC_DEVICE_NODE_HPP
 
@@ -5,7 +6,7 @@
 #include "std_srvs/srv/trigger.hpp"
 #include "std_msgs/msg/float64.hpp"
 #include "canopen_interfaces/srv/co_target_double.hpp"
-#include "canopen_proxy_driver/canopen_proxy_driver.hpp"
+#include "canopen_proxy_driver/lifecycle_canopen_proxy_driver.hpp"
 #include "canopen_402_driver/motor.hpp"
 
 using namespace std::chrono_literals;
@@ -19,7 +20,7 @@ namespace ros2_canopen
      * This class provides a ros2 node for a CIA 402
      * device.
      */
-    class MotionControllerDriver : public ProxyDriver
+    class LifecycleMotionControllerDriver : public LifecycleProxyDriver
     {
     private:
         std::shared_ptr<MCDeviceDriver> mc_driver_;
@@ -36,59 +37,118 @@ namespace ros2_canopen
         rclcpp::Service<canopen_interfaces::srv::COTargetDouble>::SharedPtr handle_set_target_service;
         rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr publish_actual_position;
         rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr publish_actual_speed;
-        rclcpp::CallbackGroup::SharedPtr timer_group;
         uint32_t period_ms_;
-        bool intialised;
-        void register_services();
+        virtual bool add() override;
+        virtual void register_ros_interface() override;
+        virtual void start_timers() override;
+        virtual void stop_timers() override;
 
     public:
-        explicit MotionControllerDriver(const rclcpp::NodeOptions &options)
-            : ProxyDriver(options)
+        explicit LifecycleMotionControllerDriver(const rclcpp::NodeOptions &options)
+            : LifecycleProxyDriver(options)
         {
-            intialised = false;
         }
 
         void run()
         {
-            if(!intialised)
-            {
-                RCLCPP_INFO(this->get_logger(), "Intitialising Device and Objects");
-                timer_->cancel();
-                intialised = true;
-                motor_->registerDefaultModes();
-                mc_driver_->validate_objs();
-                timer_= this->create_wall_timer(
-                        std::chrono::milliseconds(period_ms_), std::bind(&MotionControllerDriver::run, this), timer_group);
-            }
-
             motor_->handleRead();
             motor_->handleWrite();
-            //motor_->handleDiag();
+            // motor_->handleDiag();
             publish();
         }
 
-        void init(ev::Executor &exec,
-                  canopen::AsyncMaster &master,
-                  uint8_t node_id,
-                  std::shared_ptr<ros2_canopen::ConfigurationManager>  config) noexcept override;
+        void init() override
+        {
+            LifecycleProxyDriver::init();
+            period_ms_ = 20;
+        }
 
     protected:
+        // void read_config() override
+        // {
+        //     RCLCPP_INFO(this->get_logger(), "read_config_start");
+        //     auto period = this->config_->get_entry<uint32_t>(std::string(this->get_name()), std::string("period"));
+        //     RCLCPP_INFO(this->get_logger(), "read_config_start");
+        //     if (!period.has_value())
+        //     {
+        //         RCLCPP_ERROR(this->get_logger(), "ERROR: Bus Configuration does not set period for %s", this->get_name());
+        //     }
+        //     RCLCPP_INFO(this->get_logger(), "read_config_start");
+        //     period_ms_ = period.value();
+        //     RCLCPP_INFO(this->get_logger(), "read_config_end");
+        // }
+
         virtual void on_rpdo(COData data) override
         {
             RCLCPP_INFO(this->get_logger(), "on_rpo not implemented");
         }
 
+        /**
+         * @brief Configures the driver
+         *
+         * Read parameters
+         * Initialise objects
+         *
+         * @param state
+         * @return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+         */
+        rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+        on_configure(const rclcpp_lifecycle::State &state);
+
+        /**
+         * @brief Activates the driver
+         *
+         * Add driver to masters event loop
+         *
+         * @param state
+         * @return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+         */
+        rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+        on_activate(const rclcpp_lifecycle::State &state);
+
+        /**
+         * @brief Deactivates the driver
+         *
+         * Remove driver from masters event loop
+         *
+         * @param state
+         * @return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+         */
+        rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+        on_deactivate(const rclcpp_lifecycle::State &state);
+
+        /**
+         * @brief Cleanup the driver
+         *
+         * Delete objects
+         *
+         * @param state
+         * @return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+         */
+        rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+        on_cleanup(const rclcpp_lifecycle::State &state);
+
+        /**
+         * @brief Shutdowns the driver
+         *
+         * Delete objects
+         *
+         * @param state
+         * @return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+         */
+        rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+        on_shutdown(const rclcpp_lifecycle::State &state);
+
     private:
-        std::atomic<bool> active;
 
         /**
          * @brief Service Callback to initialise device
-         * 
+         *
          * Calls Motor402::handleInit function. Brings motor to enabled
          * state and homes it.
-         * 
-         * @param [in] request 
-         * @param [out] response 
+         *
+         * @param [in] request
+         * @param [out] response
          */
         void handle_init(
             const std_srvs::srv::Trigger::Request::SharedPtr request,
@@ -96,12 +156,12 @@ namespace ros2_canopen
 
         /**
          * @brief Service Callback to recover device
-         * 
+         *
          * Calls Motor402::handleRecover function. Resets faults and brings
          * motor to enabled state.
-         * 
-         * @param [in] request 
-         * @param [out] response 
+         *
+         * @param [in] request
+         * @param [out] response
          */
         void handle_recover(
             const std_srvs::srv::Trigger::Request::SharedPtr request,
@@ -109,13 +169,13 @@ namespace ros2_canopen
 
         /**
          * @brief Service Callback to halt device
-         * 
+         *
          * Calls Motor402::handleHalt function. Calls Quickstop. Resulting
          * Motor state depends on devices configuration specifically object
          * 0x605A.
-         * 
-         * @param [in] request 
-         * @param [out] response 
+         *
+         * @param [in] request
+         * @param [out] response
          */
         void handle_halt(
             const std_srvs::srv::Trigger::Request::SharedPtr request,
@@ -123,13 +183,13 @@ namespace ros2_canopen
 
         /**
          * @brief Service Callback to set profiled position mode
-         * 
+         *
          * Calls Motor402::enterModeAndWait with Profiled Position Mode as
          * Target Operation Mode. If successful, the motor was transitioned
          * to Profiled Position Mode.
-         * 
-         * @param [in] request 
-         * @param [out] response 
+         *
+         * @param [in] request
+         * @param [out] response
          */
         void handle_set_mode_position(
             const std_srvs::srv::Trigger::Request::SharedPtr request,
@@ -137,13 +197,13 @@ namespace ros2_canopen
 
         /**
          * @brief Service Callback to set profiled velocity mode
-         * 
+         *
          * Calls Motor402::enterModeAndWait with Profiled Velocity Mode as
          * Target Operation Mode. If successful, the motor was transitioned
          * to Profiled Velocity Mode.
-         * 
-         * @param [in] request 
-         * @param [out] response 
+         *
+         * @param [in] request
+         * @param [out] response
          */
         void handle_set_mode_velocity(
             const std_srvs::srv::Trigger::Request::SharedPtr request,
@@ -151,28 +211,27 @@ namespace ros2_canopen
 
         /**
          * @brief Service Callback to set cyclic position mode
-         * 
+         *
          * Calls Motor402::enterModeAndWait with Cyclic Position Mode as
          * Target Operation Mode. If successful, the motor was transitioned
          * to Cyclic Position Mode.
-         * 
-         * @param [in] request 
-         * @param [out] response 
+         *
+         * @param [in] request
+         * @param [out] response
          */
         void handle_set_mode_cyclic_position(
             const std_srvs::srv::Trigger::Request::SharedPtr request,
             std_srvs::srv::Trigger::Response::SharedPtr response);
 
-
         /**
          * @brief Service Callback to set cyclic velocity mode
-         * 
+         *
          * Calls Motor402::enterModeAndWait with Cyclic Velocity Mode as
          * Target Operation Mode. If successful, the motor was transitioned
          * to Cyclic Velocity Mode.
-         * 
-         * @param [in] request 
-         * @param [out] response 
+         *
+         * @param [in] request
+         * @param [out] response
          */
         void handle_set_mode_cyclic_velocity(
             const std_srvs::srv::Trigger::Request::SharedPtr request,
@@ -180,13 +239,13 @@ namespace ros2_canopen
 
         /**
          * @brief Service Callback to set profiled torque mode
-         * 
+         *
          * Calls Motor402::enterModeAndWait with Profiled Torque Mode as
          * Target Operation Mode. If successful, the motor was transitioned
          * to Profiled Torque Mode.
-         * 
-         * @param [in] request 
-         * @param [out] response 
+         *
+         * @param [in] request
+         * @param [out] response
          */
         void handle_set_mode_torque(
             const std_srvs::srv::Trigger::Request::SharedPtr request,
@@ -194,13 +253,13 @@ namespace ros2_canopen
 
         /**
          * @brief Service Callback to set target
-         * 
+         *
          * Calls Motor402::setTarget and sets the requested target value. Note
          * that the resulting movement is dependent on the Operation Mode and the
          * drives state.
-         * 
-         * @param [in] request 
-         * @param [out] response 
+         *
+         * @param [in] request
+         * @param [out] response
          */
         void handle_set_target(
             const canopen_interfaces::srv::COTargetDouble::Request::SharedPtr request,
@@ -208,7 +267,7 @@ namespace ros2_canopen
 
         /**
          * @brief Publishes actual position and speed
-         * 
+         *
          */
         void publish();
     };
--- canopen_402_driver/src/canopen_402_driver.cpp
+++ canopen_402_driver/src/canopen_402_driver.cpp
@@ -172,12 +172,12 @@ void MotionControllerDriver::register_services()
         std::bind(&MotionControllerDriver::handle_set_target, this, _1, _2));
 }
 
-void MotionControllerDriver::add(ev::Executor &exec,
+void MotionControllerDriver::init(ev::Executor &exec,
                                   canopen::AsyncMaster &master,
                                   uint8_t node_id,
                                   std::shared_ptr<ros2_canopen::ConfigurationManager>  config) noexcept
 {
-    ProxyDriver::add(exec, master, node_id, config);
+    ProxyDriver::init(exec, master, node_id, config);
     auto period = this->config_->get_entry<uint32_t>(std::string(this->get_name()), std::string("period"));
     if(!period.has_value())
     {
@@ -185,16 +185,16 @@ void MotionControllerDriver::add(ev::Executor &exec,
         return;
     }
     period_ms_ = period.value();
-    driver_.reset();
+    driver.reset();
     mc_driver_ = std::make_shared<MCDeviceDriver>(exec, master, node_id);
-    driver_ = std::static_pointer_cast<LelyBridge>(mc_driver_);
-    motor_ = std::make_shared<Motor402>(std::string("motor"), mc_driver_);
+    driver = std::static_pointer_cast<LelyBridge>(mc_driver_);
+    motor_ = std::make_shared<Motor402>(mc_driver_);
     register_services();
 
     timer_group = this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
     timer_ = this->create_wall_timer(
         2000ms, std::bind(&MotionControllerDriver::run, this), timer_group);
-    driver_->Boot();
+    driver->Boot();
     active.store(true);
 }
 
