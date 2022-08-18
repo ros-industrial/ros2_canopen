5676e4e8baba94accf69beca71060b1758a81d80
Christoph Hellmann Santos <christoph.hellmann.santos@ipa.fraunhofer.de>
2022-08-18 11:48:17 +0200

    add custom target/command and install to macro

--- lely_core_libraries/cmake/lely_core_libraries-extras.cmake
+++ lely_core_libraries/cmake/lely_core_libraries-extras.cmake
@@ -1,9 +1,34 @@
-macro(dcfgen INPUT_DIR FILE OUTPUT_DIR)
-make_directory(${OUTPUT_DIR})
-    execute_process(
-        COMMAND "dcfgen" "-d" ${OUTPUT_DIR} "-rS" ${INPUT_DIR}${FILE}
-        WORKING_DIRECTORY ${INPUT_DIR}
-        TIMEOUT 2
-        RESULT_VARIABLE dcfgen_result
-        )
+# dcfgen macro executes the dcfgen command for the specified target.
+# For this macro to work there needs to be a folder config/{target}
+# in your package. Inside that folder there needs to be the bus.yml
+# file to use for generation.
+macro(
+    dcfgen 
+    TARGET)
+
+    add_custom_target(
+        ${TARGET} ALL    
+    )
+    
+    add_custom_command(
+        TARGET ${TARGET} PRE_BUILD
+        COMMAND mkdir -p ${CMAKE_BINARY_DIR}/config/${TARGET}/
+    )
+
+    add_custom_command(
+        TARGET ${TARGET} POST_BUILD
+        COMMAND dcfgen -d ${CMAKE_BINARY_DIR}/config/${TARGET}/ -rS bus.yml
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/config/${TARGET}/
+    )
+
+    install(DIRECTORY
+        config/${TARGET}/
+        DESTINATION share/${PROJECT_NAME}/config/${TARGET}/
+    )
+  
+    install(DIRECTORY
+        ${CMAKE_BINARY_DIR}/config/${TARGET}/
+        DESTINATION share/${PROJECT_NAME}/config/${TARGET}/
+    )
+
 endmacro()
