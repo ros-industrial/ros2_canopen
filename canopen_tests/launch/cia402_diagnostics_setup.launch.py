0a228ca76085a1026c51bc9c9a960efcba03e99c
Vishnuprasad Prachandabhanu <vishnu.pbhat93@gmail.com>
2023-06-05 16:34:38 +0200

    Diagnostics test launch for proxy and cia402

--- canopen_tests/launch/cia402_diagnostics_setup.launch.py
+++ canopen_tests/launch/cia402_diagnostics_setup.launch.py
@@ -9,45 +9,59 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
     slave_eds_path = os.path.join(
-        get_package_share_directory("canopen_tests"), "config", "simple", "simple.eds"
+        get_package_share_directory("canopen_tests"),
+        "config",
+        "cia402_diagnostics",
+        "cia402_slave.eds",
     )
+
     slave_node_1 = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             [
                 os.path.join(get_package_share_directory("canopen_fake_slaves"), "launch"),
-                "/basic_slave.launch.py",
+                "/cia402_slave.launch.py",
             ]
         ),
         launch_arguments={
             "node_id": "2",
-            "node_name": "slave_node_1",
+            "node_name": "cia402_node_1",
             "slave_config": slave_eds_path,
         }.items(),
     )
-
     slave_node_2 = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             [
                 os.path.join(get_package_share_directory("canopen_fake_slaves"), "launch"),
-                "/basic_slave.launch.py",
+                "/cia402_slave.launch.py",
             ]
         ),
         launch_arguments={
             "node_id": "3",
-            "node_name": "slave_node_2",
+            "node_name": "cia402_node_2",
             "slave_config": slave_eds_path,
         }.items(),
     )
-
-    print(
-        os.path.join(
-            get_package_share_directory("canopen_tests"),
-            "config",
-            "proxy_write_sdo",
-            "master.dcf",
-        )
+    slave_node_3 = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(get_package_share_directory("canopen_fake_slaves"), "launch"),
+                "/cia402_slave.launch.py",
+            ]
+        ),
+        launch_arguments={
+            "node_id": "4",
+            "node_name": "cia402_node_4",
+            "slave_config": slave_eds_path,
+        }.items(),
     )
-
+    master_bin_path = os.path.join(
+        get_package_share_directory("canopen_tests"),
+        "config",
+        "cia402_diagnostics",
+        "master.bin",
+    )
+    if not os.path.exists(master_bin_path):
+        master_bin_path = ""
     device_container = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             [
@@ -59,18 +73,34 @@ def generate_launch_description():
             "master_config": os.path.join(
                 get_package_share_directory("canopen_tests"),
                 "config",
-                "simple",
+                "cia402_diagnostics",
                 "master.dcf",
             ),
-            "master_bin": "",
+            "master_bin": master_bin_path,
             "bus_config": os.path.join(
                 get_package_share_directory("canopen_tests"),
                 "config",
-                "simple",
+                "cia402_diagnostics",
                 "bus.yml",
             ),
             "can_interface_name": "vcan0",
         }.items(),
     )
 
-    return LaunchDescription([slave_node_1, slave_node_2, device_container])
+    diagnostics_analyzer_path = os.path.join(
+        get_package_share_directory("canopen_tests"),
+        "launch",
+        "analyzers",
+        "cia402_diagnostic_analyzer.yaml",
+    )
+
+    diagnostics_aggregator_node = launch_ros.actions.Node(
+        package="diagnostic_aggregator",
+        executable="aggregator_node",
+        output="screen",
+        parameters=[diagnostics_analyzer_path],
+    )
+
+    return LaunchDescription(
+        [slave_node_1, slave_node_2, slave_node_3, device_container, diagnostics_aggregator_node]
+    )
