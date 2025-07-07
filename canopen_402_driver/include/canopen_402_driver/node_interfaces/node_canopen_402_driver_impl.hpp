60be9be43ddc3752a040f62020c6d5a16aa6d6c7
G++ <GYKrauss@users.noreply.github.com>
2025-07-07 16:11:18 +0300

    Typo in node_canopen_402_driver_impl.hpp - "offset_pos_from_dev"
    "offset_from_to_dev" --> "offset_pos_from_dev"

--- canopen_402_driver/include/canopen_402_driver/node_interfaces/node_canopen_402_driver_impl.hpp
+++ canopen_402_driver/include/canopen_402_driver/node_interfaces/node_canopen_402_driver_impl.hpp
@@ -260,7 +260,7 @@ void NodeCanopen402Driver<rclcpp_lifecycle::LifecycleNode>::configure(bool calle
   }
   try
   {
-    offset_pos_from_dev = std::optional(this->config_["offset_from_to_dev"].as<double>());
+    offset_pos_from_dev = std::optional(this->config_["offset_pos_from_dev"].as<double>());
   }
   catch (...)
   {
